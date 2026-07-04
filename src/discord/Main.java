8567ff4ce3c3c7b3cc47f8bd11aa7bb196f259c7
oğuzhan yıldırım <oguzhanym271@gmail.com>
2026-07-04 19:31:38 +0300

    Change Discord username to 'Null'fix

--- src/discord/Main.java
+++ src/discord/Main.java
@@ -5,7 +5,7 @@ public class Main {
         try {
             Discord discord = new Discord();
             discord.webhookUrl = "https://discordapp.com/api/webhooks/1413251317895401523/JPAgF1sHI-IT4GVYZ8OfJa75eN-btjqfosSZ4GtV1RO9g4u5fBpao2mAZYchxpq1Z-mk"; // kendi webhook’un
-            discord.username = "Oguzyl.1";
+            discord.username = "Null";
             discord.content = "deneme mesajı";
             discord.avatar_url = "https://i.imgur.com/oBPXx0D.png";
           
